080ac02b8f8b2568c528beaf50b73d13f974b8b2
Chad Teitsma <aiuahx6@uswsa0138142.na.intranet.msd>
2018-12-07 16:12:43 -0500

    Defining A Rule-Aware Object Bean

---
+++ promotionenginetrail/gensrc/com/chadteitsma/promotionenginetrail/constants/GeneratedPromotionenginetrailConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Dec 7, 2018 4:08:06 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.chadteitsma.promotionenginetrail.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedPromotionenginetrailConstants
+{
+	public static final String EXTENSIONNAME = "promotionenginetrail";
+	
+	protected GeneratedPromotionenginetrailConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ promotionenginetrail/src/com/chadteitsma/promotionenginetrail/setup/PromotionenginetrailSystemSetup.java
@@ -0,0 +1,43 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.chadteitsma.promotionenginetrail.setup;
+
+import static com.chadteitsma.promotionenginetrail.constants.PromotionenginetrailConstants.PLATFORM_LOGO_CODE;
+
+import de.hybris.platform.core.initialization.SystemSetup;
+
+import java.io.InputStream;
+
+import com.chadteitsma.promotionenginetrail.constants.PromotionenginetrailConstants;
+import com.chadteitsma.promotionenginetrail.service.PromotionenginetrailService;
+
+
+@SystemSetup(extension = PromotionenginetrailConstants.EXTENSIONNAME)
+public class PromotionenginetrailSystemSetup
+{
+	private final PromotionenginetrailService promotionenginetrailService;
+
+	public PromotionenginetrailSystemSetup(final PromotionenginetrailService promotionenginetrailService)
+	{
+		this.promotionenginetrailService = promotionenginetrailService;
+	}
+
+	@SystemSetup(process = SystemSetup.Process.INIT, type = SystemSetup.Type.ESSENTIAL)
+	public void createEssentialData()
+	{
+		promotionenginetrailService.createLogo(PLATFORM_LOGO_CODE);
+	}
+
+	private InputStream getImageStream()
+	{
+		return PromotionenginetrailSystemSetup.class.getResourceAsStream("/promotionenginetrail/sap-hybris-platform.png");
+	}
+}
